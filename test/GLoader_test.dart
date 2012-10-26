5c3fb8a6b5b3ff537b76efa99fc562a9cd10fe8d
Henri Chen <henrichen@zkoss.org>
2012-10-26 18:41:35 +0800

    add test cases.

---
+++ test/GLoader_test.dart
@@ -0,0 +1,8 @@
+import 'package:rikulo_gapi/rikulo_gapi.dart';
+
+void main() {
+  gLoader.loadIPLatLng()
+    .then((Map result) => print("lat: ${result['lat']}, lng: ${result['lng']}"));
+
+  gLoader.load(GLoader.FEED, "1", {"callback": ()=>print("Feed loaded")});
+}
